158d3518074d1d147a55ad6597a1aeb465bcce79
SH44N96 <sh44n96@gmail.com>
2024-06-29 11:14:23 +0800

    OnScreen Debug Messages

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -12,7 +12,12 @@ void AItem::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Warning, TEXT("Begin Play called!"))
+	UE_LOG(LogTemp, Warning, TEXT("Begin Play called!"));
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, FString("Item OnScreen Message!"));
+	}
 }
 
 void AItem::Tick(float DeltaTime)
