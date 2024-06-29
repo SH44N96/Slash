3a27021198160580a57047cf6aadee17e2eddb30
SH44N96 <sh44n96@gmail.com>
2024-06-29 11:37:29 +0800

    Formatting Strings

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -13,14 +13,16 @@ void AItem::BeginPlay()
 	Super::BeginPlay();
 
 	UE_LOG(LogTemp, Warning, TEXT("Begin Play called!"));
-
-	if (GEngine)
-	{
-		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, FString("Item OnScreen Message!"));
-	}
 }
 
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	if (GEngine)
+	{
+		FString Name = GetName();
+		FString Message = FString::Printf(TEXT("Item Name: %s"), *Name);
+		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, Message);
+	}
 }
