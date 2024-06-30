3cac21a18656cab0a1ae882dcffd97958d3079ed
SH44N96 <sh44n96@gmail.com>
2024-06-30 10:32:17 +0800

    SetActorLocation

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -14,6 +14,8 @@ void AItem::BeginPlay()
 	Super::BeginPlay();
 
 	UWorld* World = GetWorld();
+
+	SetActorLocation(FVector(0.f, 0.f, 50.f));
 	FVector Location = GetActorLocation();
 	FVector Forward = GetActorForwardVector();
 
