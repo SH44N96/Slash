e75971c8e8d42723637e30800fc0ab97853618e7
SH44N96 <sh44n96@gmail.com>
2024-06-30 11:49:45 +0800

    The Sine Function

--- Source/Slash/Public/Items/Item.h
+++ Source/Slash/Public/Items/Item.h
@@ -12,13 +12,14 @@ class SLASH_API AItem : public AActor
 	GENERATED_BODY()
 	
 public:	
-
 	AItem();
-
 	virtual void Tick(float DeltaTime) override;
 
 protected:
-
 	virtual void BeginPlay() override;
 
+private:
+	float RunningTime;
+	float Amplitude = 0.25f;
+	float TimeConstant = 5.f;
 };
