cf73c1590fd240f3856fb05e8275b4a6a8c11bae
SH44N96 <sh44n96@gmail.com>
2024-07-02 22:58:04 +0800

    Template Functions

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -19,12 +19,12 @@ void AItem::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 	RunningTime += DeltaTime;
-	
-//	float DeltaZ = Amplitude * FMath::Sin(RunningTime * TimeConstant);
-//	AddActorWorldOffset(FVector(0.f, 0.f, DeltaZ));
 
 	DRAW_SPHERE_SingleFrame(GetActorLocation());
 	DRAW_VECTOR_SingleFrame(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 100.f);
+
+	FVector AvgVector = Avg<FVector>(GetActorLocation(), FVector::ZeroVector);
+	DRAW_POINT_SingleFrame(AvgVector);
 }
 
 float AItem::TransformedSin()
